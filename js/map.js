89d30696e5e074a0d5e97fb48429c978422af74c
nmaystre <n.d.maistrenko@gmail.com>
2017-08-29 01:31:36 +0300

    добавила зависимость инпутов

--- js/map.js
+++ js/map.js
@@ -15,6 +15,16 @@ var template = document.querySelector('#lodge-template').content;
 var dialogPanelParent = document.querySelector('#offer-dialog');
 var cardCloseBtn = dialogPanelParent.querySelector('.dialog__close');
 
+var userAdv = document.querySelector('.notice__form');
+var advTitle = userAdv.querySelector('#title');
+var advPrice = userAdv.querySelector('#price');
+var advAddress = userAdv.querySelector('#address');
+var advType = userAdv.querySelector('#type');
+var advRooms = userAdv.querySelector('#room_number');
+var advGuests = userAdv.querySelector('#capacity');
+var advCheckin = userAdv.querySelector('#timein');
+var advCheckout = userAdv.querySelector('#timeout');
+
 
 var getRandomNumber = function (min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
@@ -165,7 +175,102 @@ cardCloseBtn.addEventListener('keydown', function (evt) {
   }
 });
 
+advTitle.addEventListener('input', function (evt) {
+  var target = evt.target;
+  if (target.value.length < 30) {
+    target.setCustomValidity('Заголовок должен состоять минимум из 30 символов');
+  } else {
+    target.setCustomValidity('');
+  }
+});
+
+advAddress.addEventListener('invalid', function () {
+  if (!advAddress.validity.valid) {
+    if (advAddress.validity.valueMissing) {
+      advAddress.setCustomValidity('Обязательное поле');
+    }
+  } else {
+    advAddress.setCustomValidity('');
+  }
+});
+
+advPrice.addEventListener('invalid', function () {
+  if (!advPrice.validity.valid) {
+    if (advPrice.validity.valueMissing) {
+      advPrice.setCustomValidity('Обязательное поле');
+    }
+  } else {
+    advPrice.setCustomValidity('');
+  }
+});
+
+advCheckin.addEventListener('input', function () {
+  var newAdvCheckin = advCheckin.value;
+  advCheckout.value = newAdvCheckin;
+});
+
+advType.addEventListener('input', function () {
+  var newAdvType = advType.value;
+  switch (newAdvType) {
+    case 'flat':
+      advPrice.minLength = '1000';
+      advPrice.placeholder = '1000';
+      break;
+    case 'bungalo':
+      advPrice.minLength = '0';
+      advPrice.placeholder = '0';
+      break;
+    case 'house':
+      advPrice.minLength = '5000';
+      advPrice.placeholder = '5000';
+      break;
+    case 'palace':
+      advPrice.minLength = '10000';
+      advPrice.placeholder = '10000';
+  }
+});
+
+advRooms.addEventListener('input', function () {
+  var newAdvRooms = advRooms.value;
+  switch (newAdvRooms) {
+    case '1':
+      advGuests.value = '1';
+      break;
+    case '2':
+      advGuests.value = '2';
+      break;
+    case '3':
+      advGuests.value = '3';
+      break;
+    case '100':
+      advGuests.value = '0';
+  }
+});
+
+advGuests.addEventListener('input', function () {
+  var newAdvGuests = advGuests.value;
+  switch (newAdvGuests) {
+    case '1':
+      advRooms.value = '1';
+      break;
+    case '2':
+      advRooms.value = '2';
+      break;
+    case '3':
+      advRooms.value = '3';
+      break;
+    case '0':
+      advRooms.value = '100';
+  }
+});
+
+var customValidation = function () {
+  invalidities: [],
+    
+}
+
 for (var i = 0; i < 8; i++) {
   randomArray[i] = generateItem(i);
   pinMap.appendChild(createPin(randomArray[i]));
 }
+
