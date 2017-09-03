3bfe004e95b47d9b2d276e6962646dc1b604b5cc
nmaystre <n.d.maistrenko@gmail.com>
2017-09-03 11:54:13 +0300

    111

--- js/map.js
+++ js/map.js
@@ -1,43 +1,40 @@
 'use strict';
 
 
-window.map = (function () {
-  var pinMap = document.querySelector('.tokyo__pin-map');
-  var generateRandomArray = window.data.generateRandomArray();
-  var draggablePin = document.querySelector('.pin__main');
-  var newAdvAddress;
-  for (var i = 0; i < generateRandomArray.length; i++) {
-    pinMap.appendChild(window.pin.createPin(generateRandomArray[i]));
-  }
-  draggablePin.addEventListener('mousedown', function (evt) {
-    evt.preventDefault();
-    var startCoords = {
-      x: evt.clientX,
-      y: evt.clientY
+var pinMap = document.querySelector('.tokyo__pin-map');
+var generateNewPin = window.data.generateRandomArray();
+var draggablePin = document.querySelector('.pin__main');
+var newAdvAddress;
+for (var i = 0; i < generateNewPin.length; i++) {
+  pinMap.appendChild(window.pin.createPin(generateNewPin[i]));
+}
+draggablePin.addEventListener('mousedown', function (evt) {
+  evt.preventDefault();
+  var startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+  var onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
     };
-    var onMouseMove = function (moveEvt) {
-      moveEvt.preventDefault();
-      var shift = {
-        x: startCoords.x - moveEvt.clientX,
-        y: startCoords.y - moveEvt.clientY
-      };
-      startCoords = {
-        x: moveEvt.clientX,
-        y: moveEvt.clientY
-      };
-      draggablePin.style.top = (draggablePin.offsetTop - shift.y) + 'px';
-      draggablePin.style.left = (draggablePin.offsetLeft - shift.x) + 'px';
-      newAdvAddress = 'x: ' + draggablePin.style.top + ', y: ' + draggablePin.style.left;
-      window.form.advAddress.value = newAdvAddress;
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
     };
-    var onMouseUp = function (upEvt) {
-      upEvt.preventDefault();
-      document.removeEventListener('mousemove', onMouseMove);
-      document.removeEventListener('mouseup', onMouseUp);
-    };
-    document.addEventListener('mousemove', onMouseMove);
-    document.addEventListener('mouseup', onMouseUp);
-  });
-
-})();
+    draggablePin.style.top = (draggablePin.offsetTop - shift.y) + 'px';
+    draggablePin.style.left = (draggablePin.offsetLeft - shift.x) + 'px';
+    newAdvAddress = 'x: ' + draggablePin.style.top + ', y: ' + draggablePin.style.left;
+    window.form.advAddress.value = newAdvAddress;
+  };
+  var onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+  };
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+});
 
