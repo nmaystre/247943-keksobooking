ec800a351dc4ef70b2bf1f0663bfcb17e0ed48a6
nmaystre <n.d.maistrenko@gmail.com>
2017-09-03 18:53:24 +0300

    закончила модуль 5/2

--- js/map.js
+++ js/map.js
@@ -1,10 +1,43 @@
 'use strict';
 
+(function () {
 
-var pinMap = document.querySelector('.tokyo__pin-map');
-var pinArray = window.data.generateRandomArray();
-for (var i = 0; i < pinArray.length; i++) {
-  pinMap.appendChild(window.pin.createPin(pinArray[i]));
-}
-
-
+  var pinMap = document.querySelector('.tokyo__pin-map');
+  var pinArray = window.data.generateRandomArray();
+  var draggablePin = document.querySelector('.pin__main');
+  var newAdvAddress;
+  for (var i = 0; i < pinArray.length; i++) {
+    pinMap.appendChild(window.pin.createPin(pinArray[i]));
+  }
+  draggablePin.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+    var startCoords = {
+      x: evt.clientX - 20,
+      y: evt.clientY + 40
+    };
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+      startCoords = {
+        x: moveEvt.clientX - 20,
+        y: moveEvt.clientY + 40
+      };
+      draggablePin.style.top = (draggablePin.offsetTop - shift.y) + 'px';
+      draggablePin.style.left = (draggablePin.offsetLeft - shift.x) + 'px';
+      var pinPositionLeft = (draggablePin.offsetLeft - shift.x);
+      var pinPositionTop = (draggablePin.offsetTop - shift.y);
+      newAdvAddress = 'x: ' + pinPositionLeft + ', y: ' + pinPositionTop;
+      window.form.advAddress.value = newAdvAddress;
+    };
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  });
+})();
