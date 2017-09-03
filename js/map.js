366d4db79b24c43b01a4814ebce6338805c877a7
nmaystre <n.d.maistrenko@gmail.com>
2017-09-03 12:13:33 +0300

    закончила модуль (но это неточно)) 3

--- js/map.js
+++ js/map.js
@@ -2,9 +2,9 @@
 
 
 var pinMap = document.querySelector('.tokyo__pin-map');
-var generateNewPin = window.data.generateRandomArray();
-for (var i = 0; i < generateNewPin.length; i++) {
-  pinMap.appendChild(window.pin.createPin(generateNewPin[i]));
+var pinArray = window.data.generateRandomArray();
+for (var i = 0; i < pinArray.length; i++) {
+  pinMap.appendChild(window.pin.createPin(pinArray[i]));
 }
 
 
