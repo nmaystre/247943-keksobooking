c2a145eb6bc29db219c1dc8a10d4ba31bf185865
nmaystre <n.d.maistrenko@gmail.com>
2017-09-05 11:22:48 +0300

    исправила события в обработчиках событий нажатия клавиш

--- js/card.js
+++ js/card.js
@@ -2,8 +2,6 @@
 
 window.card = (function () {
   var template = document.querySelector('#lodge-template').content;
-  var dialogPanelParent = document.querySelector('#offer-dialog');
-  var cardCloseBtn = dialogPanelParent.querySelector('.dialog__close');
   var createCard = function (obj) {
     var dialogPanelContent = template.cloneNode(true);
     dialogPanelContent.querySelector('.lodge__title').innerHTML = obj.offer.title;
@@ -22,55 +20,25 @@ window.card = (function () {
       default:
         lodgeType = 'Хз что это';
     }
-
     dialogPanelContent.querySelector('.lodge__type').textContent = lodgeType;
     dialogPanelContent.querySelector('.lodge__rooms-and-guests').textContent = 'Для ' + obj.offer.guests + ' гостей в ' + obj.offer.rooms + ' комнатах';
     dialogPanelContent.querySelector('.lodge__checkin-time').textContent = 'Заезд после ' + obj.offer.checkin + ' выезд до ' + obj.offer.checkout;
-
     for (var i = 0; i < obj.offer.features.length; i++) {
       var featuresSpan = document.createElement('span');
       featuresSpan.classList.add('feature__image', 'feature__image--' + obj.offer.features[i]);
       dialogPanelContent.querySelector('.lodge__features').appendChild(featuresSpan);
     }
-
     var templateAvatar = document.querySelector('.lodge__description');
     var lodgeDescriptionContent = templateAvatar.cloneNode(true);
     lodgeDescriptionContent.textContent = obj.offer.description;
-
     var dialogTitle = document.querySelector('.dialog__title');
     var dialogTitleContent = dialogTitle.children[0];
     dialogTitleContent.src = obj.author.avatar;
-
-    document.addEventListener('keydown', onCardEscPress);
+    document.addEventListener('keydown', window.showcard.onCardEscPress);
     return dialogPanelContent;
   };
 
-
-  var onCardEscPress = function (evt) {
-    window.util.isEscEvent(evt, closeCard);
-
-  };
-
-  var closeCard = function () {
-    dialogPanelParent.classList.add('hidden');
-    document.removeEventListener('keydown', onCardEscPress);
-  };
-
-  cardCloseBtn.addEventListener('click', function () {
-    closeCard();
-  });
-
-  document.addEventListener('keydown', function (evt) {
-    window.util.isEscEvent(evt, closeCard);
-
-  });
-
-  cardCloseBtn.addEventListener('keydown', function (evt) {
-    window.util.isEnterEvent(evt, closeCard);
-
-  });
   return {
     createCard: createCard
   };
-
 })();
--- js/show-card.js
+++ js/show-card.js
@@ -1,17 +1,39 @@
 'use strict';
 
 window.showcard = (function () {
-  var showCard = function (element, onClose) {
-    var dialogPanelParent = document.querySelector('#offer-dialog');
+  var dialogPanelParent = document.querySelector('#offer-dialog');
+  var cardCloseBtn = dialogPanelParent.querySelector('.dialog__close');
+
+  var showCard = function (element) {
     dialogPanelParent.classList.remove('hidden');
     var newContent = window.card.createCard(element);
     var dialogPanel = document.querySelector('.dialog__panel');
     dialogPanelParent.replaceChild(newContent, dialogPanel);
+  };
+
+  var closeCard = function (onClose) {
+    dialogPanelParent.classList.add('hidden');
+    document.removeEventListener('keydown', onCardEscPress);
     if (typeof onClose === 'function') {
       onClose();
     }
   };
+
+  cardCloseBtn.addEventListener('click', function () {
+    closeCard();
+  });
+  document.addEventListener('keydown', function (evt) {
+    window.util.isEscEvent(evt, closeCard);
+  });
+  cardCloseBtn.addEventListener('keydown', function (evt) {
+    window.util.isEnterEvent(evt, closeCard);
+  });
+  var onCardEscPress = function (evt) {
+    window.util.isEscEvent(evt, closeCard);
+  };
+
   return {
-    showCard: showCard
+    showCard: showCard,
+    onCardEscPress: onCardEscPress
   };
 })();
